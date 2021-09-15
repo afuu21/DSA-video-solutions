9b132e1c82d87f77186402f14f20f17515a5aa80
Mohammed Afnan Ahmed <55314442+afuu21@users.noreply.github.com>
2021-09-15 11:21:57 +0530

    Create EvenOrOdd.java

---
+++ 02-first-java/EvenOrOdd.java
@@ -0,0 +1,16 @@
+//Video link: https://youtu.be/a8jbf81buj4
+import java.util.*;
+public class EvenOrOdd {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number: ");
+		int n = sc.nextInt();
+		if(n%2==0)
+			System.out.println("Even");
+		else
+			System.out.println("Odd");
+		
+	}
+	
+	
+}
