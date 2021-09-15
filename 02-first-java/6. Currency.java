a171d24b61d7f52c6497690a83248869b4b0f805
Mohammed Afnan Ahmed <55314442+afuu21@users.noreply.github.com>
2021-09-15 11:43:32 +0530

    Create 6. Currency.java

---
+++ 02-first-java/6. Currency.java
@@ -0,0 +1,12 @@
+//Video link: 
+import java.util.*;
+public class DigitToString {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a;
+		a = sc.nextInt();
+		System.out.println("USD: " + a*78);
+	}
+	
+	
+}
