2f4bd38d53b47dbf773bf2fa28a1a2de26dd9afa
Mohammed Afnan Ahmed <55314442+afuu21@users.noreply.github.com>
2021-09-15 11:27:09 +0530

    Create NameAndGreet.java

---
+++ 02-first-java/NameAndGreet.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class NameAndGreet {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String name;
+		System.out.println("Enter your name: ");
+		name = sc.next();
+		
+		System.out.println("Hello, " + name);
+		
+	}
+	
+	
+}
