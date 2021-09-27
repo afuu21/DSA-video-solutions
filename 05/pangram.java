b403140e057fa9c9d6a5061ea6f6aea83b6329a2
Mohammed Afnan Ahmed <55314442+afuu21@users.noreply.github.com>
2021-09-27 20:57:16 +0530

    Delete 05 directory

--- 05/pangram.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public boolean checkIfPangram(String sentance){
-        Set<Character> set = new HashSet<>();
-        for(int i=0; i<sentance.length(); i++) {
-            set.add(sentance.charAt(i));
-        }
-        return set.size()== 26;
-    }
-}
